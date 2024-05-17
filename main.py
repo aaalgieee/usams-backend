c1493c7b8f4d23d715d576c5c33a038407726268
Al Gabriel Orig <jaworski.orig@gmail.com>
2024-05-18 06:41:11 +0800

    add: func

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from modules import models
 from modules.database import SessionLocal, engine
-from modules.models import Account
-from pydantic import BaseModel
+from modules.models import Account, Activity, Attendance, EventLog
+from pydantic import BaseModel, Field
+from datetime import date, datetime
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -19,6 +21,17 @@ def get_db():
     finally:
         db.close()
 
+def log_event(db: Session, account_id: int, event_description: str, ip: str):
+    try:
+        event = EventLog(account_id=account_id, event_description=event_description, ip=ip, datetime_added=datetime.now())
+        db.add(event)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+
+        # passively ignore error
+        pass
+
 # Endpoints
 @app.get("/test/users")
 def get_account(db: Session = Depends(get_db)):
@@ -29,6 +42,20 @@ class User(BaseModel):
     username: str
     password: str
 
+class Event(BaseModel):
+    # activity_id: Optional[int]
+    # semester_id: Optional[int]
+    code: str
+    label: str
+    # description: Optional[str]
+    location: str
+    # college_id: Optional[int]
+    activity_start_date: date
+    activity_end_date: date
+    activity_status: Optional[str] = Field(default="Active")
+    datetime_created: Optional[datetime] = Field(default=datetime.now())
+    # approved_by: Optional[int]
+
 @app.post("/login")
 def login(user: User, db: Session = Depends(get_db)):
     account = db.query(Account).filter(Account.username == user.username).first()
@@ -36,6 +63,81 @@ def login(user: User, db: Session = Depends(get_db)):
         raise HTTPException(status_code=401, detail="Invalid username or password")
     return {"message": "Login successful"}
 
+@app.post("/register")
+def register(user: User, db: Session = Depends(get_db)):
+    account = Account(**user.dict())
+    db.add(account)
+    db.commit()
+    return {"message": "Registration successful"}
+
+@app.get("/events")
+def get_events(db: Session = Depends(get_db)):
+    events = db.query(Activity).all()
+    return events
+
+@app.post("/events")
+def add_event(evt: Event, db: Session = Depends(get_db)):
+    activity = Activity(**evt.model_dump())
+    # add approved_by default admin user to activity
+    activity.approved_by = 1
+
+    db.add(activity)
+    db.commit()
+    return {"message": "Event added"}
+
+@app.patch("/events/{activity_id}")
+def update_event(activity_id: int, activity: Event, db: Session = Depends(get_db)):
+    db.query(Activity).filter(Event.activity_id == activity_id).update(activity.dict())
+    db.commit()
+    return {"message": "Event updated"}
+
+@app.delete("/events/{activity_id}")
+def delete_event(activity_id: int, db: Session = Depends(get_db)):
+    activity = db.query(Activity).filter(Activity.activity_id == activity_id).first()
+    if not activity:
+        raise HTTPException(status_code=404, detail="Event not found")
+    db.delete(activity)
+    db.commit()
+    return {"message": "Event deleted"}
+
+@app.get("/attendance")
+def get_attendance(db: Session = Depends(get_db)):
+    attendance = db.query(Attendance).all()
+    return attendance
+
+class _Attendance(BaseModel):
+    activity_id: int
+    account_id: int
+    attendance_status: str = Field(default="Present")
+    datetime_created: datetime = Field(default=datetime.now())
+
+@app.post("/attendance")
+def add_attendance(att: _Attendance, db: Session = Depends(get_db)):
+    attendance = Attendance(**att.dict())
+    db.add(attendance)
+    db.commit()
+
+    log_event(db, att.account_id, f"Added attendance for activity {att.activity_id}", "")
+    return {"message": "Attendance added"}
+
+@app.patch("/attendance/{attendance_id}")
+def update_attendance(attendance_id: int, att: _Attendance, db: Session = Depends(get_db)):
+    db.query(Attendance).filter(Attendance.attendance_id == attendance_id).update(att.dict())
+    db.commit()
+
+    log_event(db, att.account_id, f"Updated attendance for activity {att.activity_id}", "")
+    return {"message": "Attendance updated"}
+
+@app.delete("/attendance/{attendance_id}")
+def delete_attendance(attendance_id: int, db: Session = Depends(get_db)):
+    attendance = db.query(Attendance).filter(Attendance.attendance_id == attendance_id).first()
+    if not attendance:
+        raise HTTPException(status_code=404, detail="Attendance not found")
+    db.delete(attendance)
+    db.commit()
+
+    log_event(db, attendance.account_id, f"Deleted attendance for activity {attendance.activity_id}", "")
+    return {"message": "Attendance deleted"}
 
 app.add_middleware(
     CORSMiddleware,
@@ -44,3 +146,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", port=8000, reload=True)
--- modules/models.py
+++ modules/models.py
@@ -3,24 +3,133 @@ from pydantic import BaseModel
 
 from .database import Base
 
+# class Account(Base):
+#     __tablename__ = "account"
+#     account_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+#     firstname = sqlalchemy.Column(sqlalchemy.String)
+#     lastname = sqlalchemy.Column(sqlalchemy.String)
+#     middlename = sqlalchemy.Column(sqlalchemy.String)
+#     username = sqlalchemy.Column(sqlalchemy.String, unique=True)
+#     password = sqlalchemy.Column(sqlalchemy.String)
+#     def verify_password(self, password: str) -> bool:
+#         # Add your password verification logic here
+#         return self.password == password
+
 class Account(Base):
-    __tablename__ = "account"
-    account_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
-    firstname = sqlalchemy.Column(sqlalchemy.String)
-    lastname = sqlalchemy.Column(sqlalchemy.String)
-    middlename = sqlalchemy.Column(sqlalchemy.String)
-    username = sqlalchemy.Column(sqlalchemy.String, unique=True)
-    password = sqlalchemy.Column(sqlalchemy.String)
+    __tablename__ = 'account'
+    account_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    username = sqlalchemy.Column(sqlalchemy.String(255), nullable=False, unique=True)
+    password = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    lastname = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    firstname = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    middlename = sqlalchemy.Column(sqlalchemy.String(255))
+    datetime_created = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    # statuscode = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('statuscode.statuscode'), nullable=False)
+    # emp_account = sqlalchemy.Column(sqlalchemy.Integer)
+    # statuscode_rel = sqlalchemy.orm.relationship('Statuscode')
+
     def verify_password(self, password: str) -> bool:
         # Add your password verification logic here
         return self.password == password
 
+class StatuscodeGroup(Base):
+    __tablename__ = 'statuscode_group'
+    statuscode_group_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+    table_name = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    range_start = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    range_end = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+
+class Statuscode(Base):
+    __tablename__ = 'statuscode'
+    statuscode_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    label = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    statuscode_group_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('statuscode_group.statuscode_group_id'), nullable=False)
 
 class Activity(Base):
-    __tablename__ = "activity"
-    activity_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
-    account_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("account.account_id"))
-    activity = sqlalchemy.Column(sqlalchemy.String)
-    date = sqlalchemy.Column(sqlalchemy.Date)
-    time = sqlalchemy.Column(sqlalchemy.Time)
-    account = sqlalchemy.orm.relationship("Account", back_populates="activity")
+    __tablename__ = 'activity'
+    activity_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    semester_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    code = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    label = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    description = sqlalchemy.Column(sqlalchemy.Text)
+    location = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    college_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    activity_start_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    activity_end_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    activity_status = sqlalchemy.Column(sqlalchemy.Enum('Active', 'Inactive'), default='Active')
+    approved_by = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("account.account_id"))
+    datetime_approved = sqlalchemy.Column(sqlalchemy.DateTime)
+    disapproved_by = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("account.account_id"))
+    disapproval_remarks = sqlalchemy.Column(sqlalchemy.Text)
+    datetime_disapproved = sqlalchemy.Column(sqlalchemy.DateTime)
+    datetime_created = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    datetime_updated = sqlalchemy.Column(sqlalchemy.DateTime)
+    statuscode = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("statuscode.statuscode_id"))
+    # statuscode_rel = sqlalchemy.orm.relationship('Statuscode')
+
+# class Activity(Base):
+#     __tablename__ = "activity"
+#     activity_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+#     account_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("account.account_id"))
+#     activity = sqlalchemy.Column(sqlalchemy.String)
+#     date = sqlalchemy.Column(sqlalchemy.Date)
+#     time = sqlalchemy.Column(sqlalchemy.Time)
+#     account = sqlalchemy.orm.relationship("Account", back_populates="activity")
+
+class TransactionType(Base):
+    __tablename__ = 'transaction_type'
+    transaction_type_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    label = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+
+class Role(Base):
+    __tablename__ = 'role'
+    role_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    label = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    description = sqlalchemy.Column(sqlalchemy.Text)
+    datetime_created = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+
+class AccountRole(Base):
+    __tablename__ = 'account_role'
+    account_role_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    account_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('account.account_id'), nullable=False)
+    role_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('role.role_id'), nullable=False)
+    granter_account = sqlalchemy.Column(sqlalchemy.Integer)
+    account = sqlalchemy.orm.relationship('Account')
+    role = sqlalchemy.orm.relationship('Role')
+
+class AccountSession(Base):
+    __tablename__ = 'account_session'
+    account_session_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    account_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('account.account_id'), nullable=False)
+    session_key = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    ip = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    datetime_created = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    datetime_last_request = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    account = sqlalchemy.orm.relationship('Account')
+
+class Attendance(Base):
+    __tablename__ = 'attendance'
+    attendance_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    activity_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('activity.activity_id'), nullable=False)
+    activity_code = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    card_id = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    student_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    employee_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    student_number = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    transaction_type_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('transaction_type.transaction_type_id'), nullable=False)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('account.account_id'), nullable=False)
+    ip_address = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    datetime_created = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    date_created = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    activity = sqlalchemy.orm.relationship('Activity')
+    transaction_type = sqlalchemy.orm.relationship('TransactionType')
+    user = sqlalchemy.orm.relationship('Account')
+
+class EventLog(Base):
+    __tablename__ = 'event_log'
+    event_log_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    account_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('account.account_id'), nullable=False)
+    event_description = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
+    ip = sqlalchemy.Column(sqlalchemy.String(255), nullable=False)
+    datetime_added = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False)
+    account = sqlalchemy.orm.relationship('Account')
